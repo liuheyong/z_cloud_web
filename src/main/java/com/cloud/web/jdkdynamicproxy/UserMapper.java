58b0e8f27ab3e6a0ac195f5110e01762c2d386e3
liuheyong <17682347237@163.com>
2019-08-16 11:42:01 +0800

    fix

--- src/main/java/com/cloud/web/jdkdynamicproxy/UserMapper.java
+++ src/main/java/com/cloud/web/jdkdynamicproxy/UserMapper.java
@@ -1,14 +1,14 @@
-﻿package com.cloud.web.jdkdynamicproxy;
-
-import com.cloud.commons.dto.Student;
-
-/**
- * @author: HeYongLiu
- * @create: 08-15-2019
- * @description: 测试mapper
- **/
-public interface UserMapper {
-
-    public Student getUserById(Integer id);
-
-}
+﻿//package com.cloud.web.jdkdynamicproxy;
+//
+//import com.cloud.commons.dto.Student;
+//
+///**
+// * @author: HeYongLiu
+// * @create: 08-15-2019
+// * @description: 测试mapper
+// **/
+//public interface UserMapper {
+//
+//    public Student getUserById(Integer id);
+//
+//}
