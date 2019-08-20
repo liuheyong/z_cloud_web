55e6b165063559d2c4f2d4ab607b7e718a53db78
liuheyong <17682347237@163.com>
2019-08-20 09:47:32 +0800

    2019年8月20日09:47:24

--- src/main/java/com/cloud/web/web/DefaultController.java
+++ src/main/java/com/cloud/web/web/DefaultController.java
@@ -2,7 +2,6 @@ package com.cloud.web.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.Serializable;
@@ -13,7 +12,6 @@ import java.io.Serializable;
  * @exception:
  * @description: defaultcontroller
  **/
-@Controller
 public class DefaultController implements Cloneable, Serializable {
 
     public static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
@@ -21,7 +19,7 @@ public class DefaultController implements Cloneable, Serializable {
     private static final long serialVersionUID = 1L;
 
     @RequestMapping("/test")
-    public String test() throws Exception {
+    public String test() {
         return "hello";
     }
 
