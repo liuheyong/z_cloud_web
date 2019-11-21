2e4c03f701dead8257a70e8aa52491e6fda4b484
liuheyong <17682347237@163.com>
2019-11-21 15:39:11 +0800

    域名提取

--- src/main/java/com/cloud/web/web/DefaultController.java
+++ src/main/java/com/cloud/web/web/DefaultController.java
@@ -3,6 +3,7 @@ package com.cloud.web.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 
 /**
@@ -16,5 +17,15 @@ public class DefaultController implements Cloneable, Serializable {
     public static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
     private static final long serialVersionUID = 1L;
+
+    /**
+     * @description: 域名提取 例：https://www.baidu.com/s?ie=UTF-8&wd=devv 提取之后返回 www.baidu.com
+     */
+    protected String domainUrlExtract(HttpServletRequest request) {
+        StringBuffer requestURL = request.getRequestURL();
+        String[] split = requestURL.toString().split("//");
+        String[] split2 = split[1].split("/");
+        return split2[0];
+    }
 
 }
