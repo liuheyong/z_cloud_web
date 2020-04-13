510d7052598e1b19e3996ead6e43d65224478ab9
liuheyong <17682347237@163.com>
2020-04-14 00:54:36 +0800

    Ω

---
+++ src/main/java/com/cloud/web/algorithm/DualPivotQuickSortAlgorithm.java
@@ -0,0 +1,81 @@
+package com.cloud.web.algorithm;
+
+import java.util.*;
+
+/**
+ * @author: LiuHeYong
+ * @create: 2020-04-13
+ * @description: 双基准快速排序算法
+ */
+public class DualPivotQuickSortAlgorithm {
+
+    /**
+    * @Date:  2020-04-13
+    * @Description: 双基准快速排序--选取 A[left], A[right] 两个数为主元
+    */
+    public static void dualPivotQuickSort(int[] A, int left, int right) {
+        if (right - left >= 1) {
+            int p = minTwoNum(A[left], A[right]);
+            int q = maxTwoNum(A[left], A[right]);
+            int ℓ = left + 1;
+            int g = right - 1;
+            int k = ℓ;
+            while (k <= g) {
+                if (A[k] < p) {
+                    A[k] = A[k] ^ A[ℓ];
+                    A[ℓ] = A[k] ^ A[ℓ];
+                    A[k] = A[k] ^ A[ℓ];
+                    ℓ = ℓ + 1;
+                } else if (A[k] >= q) {
+                    while (A[g] > q && k < g) {
+                        g = g - 1;
+                    }
+                    A[k] = A[k] ^ A[g];
+                    A[g] = A[k] ^ A[g];
+                    A[k] = A[k] ^ A[g];
+                    g--;
+                    if (A[k] < p) {
+                        A[k] = A[k] ^ A[ℓ];
+                        A[ℓ] = A[k] ^ A[ℓ];
+                        A[k] = A[k] ^ A[ℓ];
+                        ℓ = ℓ + 1;
+                    }
+                }
+                k++;
+            }
+            ℓ--;
+            g++;
+            A[left] = A[ℓ];
+            A[ℓ] = p;
+            A[right] = A[g];
+            A[g] = q;
+            dualPivotQuickSort(A, left, ℓ - 1);
+            dualPivotQuickSort(A, ℓ + 1, g - 1);
+            dualPivotQuickSort(A, g + 1, right);
+        }
+    }
+
+    public static void swap(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
+
+    private static int maxTwoNum(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    private static int minTwoNum(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    static int[] a = {133, 121, -12, 14, 534, -23, 87, -87, 81, 41};
+
+    public static void main(String[] args) {
+
+        dualPivotQuickSort(a, 0, 9);
+        System.out.println(Arrays.toString(a));
+
+    }
+
+}
--- src/main/java/com/cloud/web/algorithm/leecode/algorithmPracticeString.java
+++ src/main/java/com/cloud/web/algorithm/leecode/algorithmPracticeString.java
@@ -8,9 +8,9 @@ package com.cloud.web.algorithm.leecode;
 public class algorithmPracticeString {
 
     /**
-    * @Date:  2020-04-09
-    * @Description: 反转字符串
-    */
+     * @Date: 2020-04-09
+     * @Description: 反转字符串
+     */
     public static void reverseString(char[] s) {
         if (s == null || s.length == 0) {
             System.out.println("数组为空");
@@ -22,6 +22,28 @@ public class algorithmPracticeString {
             str += "\"" + s[i] + "\",";
         }
         System.out.println("[" + str.substring(0, str.lastIndexOf(",")) + "]");
+    }
+
+    /**
+     * @Date: 2020-04-09
+     * @Description: 反转带符号的Integer范围内的整数
+     */
+    public static int reverse(int x) {
+        //存放结果
+        int rev = 0;
+        while (x != 0) {
+            //每次取最后一位数
+            int temp = x % 10;
+            //每次取完最后一位后将其去掉
+            x /= 10;
+            //溢出零界点判断 范围[-2^31 , 2^31 -1] 即 [-2147483648，2147483647]
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && temp > 7))
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && temp < -8))
+                return 0;
+            rev = rev * 10 + temp;
+        }
+        return rev;
     }
 
     static char[] s = {'H', '2', 'n', 'e', 'a', 'h'};
