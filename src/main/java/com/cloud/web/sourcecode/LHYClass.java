4b63d61fb11161300501f2b644893baf89248fff
liuheyong <17682347237@163.com>
2020-06-08 14:27:42 +0800

    生成身份证号

---
+++ src/main/java/com/cloud/web/sourcecode/LHYClass.java
@@ -0,0 +1,26 @@
+package com.cloud.web.sourcecode;
+
+import com.cloud.web.web.DefaultController;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: HeYongLiu
+ * @create: 05-26-2020
+ * @description:
+ **/
+@Component
+public class LHYClass extends DefaultController implements InitializingBean, Ordered {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.debug("========================LHYClass========================");
+    }
+
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
