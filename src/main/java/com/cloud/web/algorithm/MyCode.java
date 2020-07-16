4295fd109063b3de1ffcc6d831dd2e654a2a6c31
liuheyong <17682347237@163.com>
2020-07-16 17:41:27 +0800

    2020年07月16日17:41:22

---
+++ src/main/java/com/cloud/web/algorithm/MyCode.java
@@ -0,0 +1,33 @@
+package com.cloud.web.algorithm;
+
+/**
+ * @author: wenyixicodedog
+ * @create: 2020-06-30
+ * @description:
+ */
+public class MyCode {
+
+    public MyCode() {
+    }
+
+    public static int getTotal(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n < 3) {
+            return n;
+        }
+        if (n % 3 == 0) {
+            return n + getTotal(n / 3);
+        }
+        if (n % 3 != 0) {
+            return n + getTotal(n / 3);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getTotal(13));
+    }
+
+}
